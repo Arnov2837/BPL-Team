dd8e649674ad3f11f6ef149744938a3450fac60e
Arnov2837 <Shohanurroshid2@gmail.com>
2026-07-19 04:17:27 +0300

    added all file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Nav from './Component/Navbar/Nav'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav></Nav>
+      
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Navbar/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import image from '../../assets/logo.png';
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm flex justify-between ">
+      <div className="">
+          <img src={image} alt=''></img>
+      </div>
+      <div>
+        <ul className='flex gap-7 font-bold text-1xl '>
+          <li>Home</li>
+          <li>Fixture</li>
+          <li>Teams</li>
+          <li>Schedules</li>
+            
+          
+        </ul>
+      </div>
+     <div className="">
+        <button className='font-bold'>0 Coin</button>
+     </div>
+     </div>
+    </div>
+  );
+};
+
+export default Nav;
