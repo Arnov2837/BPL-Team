059ead5695b67c2030daae77bee9266079e630a7
Arnov2837 <Shohanurroshid2@gmail.com>
2026-07-19 05:05:59 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Nav from './Component/Navbar/Nav'
 import './App.css'
+import Hero from './Component/Home/Hero'
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     <Nav></Nav>
-      
+      <Hero></Hero>
 
       
     </>
---
+++ src/Component/Home/Hero.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import image from '../../assets/banner-main.png';
+import imagee from "../../assets/bg-shadow.png"
+
+
+const Hero = () => {
+  return (
+    <>
+    <div
+  className="hero min-h-screen">
+  
+  <div className="hero-overlay"></div>
+  <div className="hero-content text-neutral-content text-center">
+    <div className="max-w-md"
+    
+     style={{
+        backgroundImage: `{imagee}`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        
+      }}
+    
+    >
+      <img className='m-auto'  src={image} alt=''></img>
+      <h1 className="mb-5 text-2xl m-1 font-bold">Assemble Your Dream 11 Cricket Team</h1>
+      <p className="mb-5">
+        Beyond Boundaries Beyond Limits
+      </p>
+      <button className="btn btn-secondary">Claim Free Credit</button>
+    </div>
+  </div>
+</div>
+    
+    
+    
+    
+    </>
+  );
+};
+
+export default Hero;
