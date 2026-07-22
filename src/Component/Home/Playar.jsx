75edb9b341e5fa404454f0351e1cfb95278a8a43
Arnov2837 <Shohanurroshid2@gmail.com>
2026-07-22 04:35:11 +0300

    add togoling

--- src/Component/Home/Playar.jsx
+++ src/Component/Home/Playar.jsx
@@ -1,17 +1,22 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers';
 
 const Playar = ({PlayerPromis}) => {
   const data = use(PlayerPromis);
   // console.log(data);
+  const [selected,setSelected]= useState("avalivale");
   
   return (
     <>
      <div className='container mx-auto flex justify-between py-3'>
       <p className='text-2xl font-bold'>Avalivale Player</p>
       <div>
-        <button className="btn btn-soft btn-primary">Primary</button>
-        <button className="btn btn-soft btn-primary">Primary</button>
+        <button onClick={() =>setSelected("avalivale")}
+        className={`btn ${selected === "avalivale" ? "bg-green-500" : ""} btn-soft btn-primary`}>Avalivale
+        </button>
+        <button onClick={() => setSelected ("selected")}
+         className={`btn ${selected === "selected" ? "bg-green-500" : ""} btn-soft btn-primary`}>selected (0)
+         </button>
       </div>
      </div>
 
